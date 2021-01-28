bce62ecea7c97ace330c8961a9bb7eb0bd9441df
dxghost <ghost@dx.dx>
2021-01-28 19:40:36 +0330

    Implement dual gradient energy function

--- energy_calculators/dual_gradient/energy.py
+++ energy_calculators/dual_gradient/energy.py
@@ -1,4 +1,6 @@
 from factory import EnergyFactory
+import numpy as np
+import math
 
 
 class DualGradient(EnergyFactory):
@@ -6,8 +8,21 @@ class DualGradient(EnergyFactory):
         pass
 
     def compute(self, rgb_matrix):
-        return [
-            [0, 0         , 0],
-            [0, 52204**0.5, 0],
-            [0, 0         , 0]
-        ]
+        x_len = len(rgb_matrix[0])
+        y_len = len(rgb_matrix)
+        energy = np.zeros((y_len, x_len))
+        for i in range(1, y_len-1):
+            for j in range(1, x_len-1):
+                r2x = rgb_matrix[i][j+1][0] - rgb_matrix[i][j-1][0]
+                b2x = rgb_matrix[i][j+1][1] - rgb_matrix[i][j-1][1]
+                g2x = rgb_matrix[i][j+1][2] - rgb_matrix[i][j-1][2]
+                deltax = math.pow(r2x, 2) + math.pow(b2x, 2) + math.pow(g2x, 2)
+
+                r2y = rgb_matrix[i+1][j][0] - rgb_matrix[i-1][j][0]
+                b2y = rgb_matrix[i+1][j][1] - rgb_matrix[i-1][j][1]
+                g2y = rgb_matrix[i+1][j][2] - rgb_matrix[i-1][j][2]
+                deltay = math.pow(r2y, 2) + math.pow(b2y, 2) + math.pow(g2y, 2)
+
+                energy[i][j] = math.sqrt(int(deltax+deltay))
+
+        return energy
--- energy_calculators/test_energy_calculator.py
+++ energy_calculators/test_energy_calculator.py
@@ -1,13 +1,13 @@
 from calculator import EnergyCalculator
 import unittest
-
+import math
 
 class TestCalculator(unittest.TestCase):
 
     def test_dual_gradient(self):
         self.test_data = [
             [(0,   0,   0), (255, 153, 153), (0,   0,   0)],
-            [(255, 204,  51), (255, 204, 153), (255, 205, 255)],
+            [(255, 203,  51), (255, 204, 153), (255, 205, 255)],
             [(0,   0,   0), (255, 255, 153), (0,   0,   0)]
         ]
         calc = EnergyCalculator(self.test_data, "dg")
@@ -16,7 +16,7 @@ class TestCalculator(unittest.TestCase):
         print(self.test_data)
         print("\nresult:")
         print(result)
-        self.assertEqual(result[1][1],52204**0.5)
+        self.assertEqual(result[1][1],math.sqrt(52024))
 
 if __name__ == "__main__":
     unittest.main()
